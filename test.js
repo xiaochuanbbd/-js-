2357a35f837f73b15b1e4fa21ed5277873877c12
xiaochuan <1418122606@qq.com>
2022-05-08 20:44:53 +0800

    添加笔记到promise

--- test.js
+++ test.js
@@ -599,42 +599,667 @@
 // console.log(pow(2,3));
 // 2* (2,3-1) = 4
 // 4* (2-1) =   
-let company = { // 是同一个对象，简洁起见被压缩了
-  sales: [{
-    name: 'John',
-    salary: 1000
-  }, {
-    name: 'Alice',
-    salary: 1600
-  }],
-  development: {
-    sites: [{
-      name: 'Peter',
-      salary: 2000
-    }, {
-      name: 'Alex',
-      salary: 1800
-    }],
-    internals: [{
-      name: 'Jack',
-      salary: 1300
-    }]
-  }
-};
-let a = sumSalaries(company)
-console.log(a);
+// let company = { // 是同一个对象，简洁起见被压缩了
+//   sales: [{
+//     name: 'John',
+//     salary: 1000
+//   }, {
+//     name: 'Alice',
+//     salary: 1600
+//   }],
+//   development: {
+//     sites: [{
+//       name: 'Peter',
+//       salary: 2000
+//     }, {
+//       name: 'Alex',
+//       salary: 1800
+//     }],
+//     internals: [{
+//       name: 'Jack',
+//       salary: 1300
+//     }]
+//   }
+// };
+// let a = sumSalaries(company)
+// console.log(a);
 
-function sumSalaries(department) {
-  if (Array.isArray(department)) {
-    return department.reduce((price, i) => {
-      return price += i.salary
-    }, 0)
-  } else {
-    let sun = 0
-    for (const subdep of Object.values(department)) {
-      sun += sumSalaries(subdep)
-    }
-    return sun
+// function sumSalaries(department) {
+//   if (Array.isArray(department)) {
+//     return department.reduce((price, i) => {
+//       return price += i.salary
+//     }, 0)
+//   } else {
+//     let sun = 0
+//     for (const subdep of Object.values(department)) {
+//       sun += sumSalaries(subdep)
+//     }
+//     return sun
 
-  }
+//   }
+// }
+// function sum(a){
+//   let num =a
+//   function f (b){
+//     num +=b
+//     return f
+//   }
+//   f.toString = function(){
+//     return num
+//   }
+//   return f
+// }
+// console.log(sum(1)(2) == 4);
+//  function func(){
+//    let value = '13'
+//    return fun
+//  }
+//  func()()
+//  var value = '13'
+//  let fun = new Function('console.log(value)')
+//  fun()
+//  function func(){
+//   let value = '13'
+//   let fun = function(){
+//     console.log(value);
+//   }
+//   return fun
+// }
+// func()()
+// let fun1 = function (phres,who){
+
+//   console.log(phres,who);
+// }
+// let fun2 = function (){
+//   console.log(2);
+// }
+// let Tid = setTimeout(fun1, 1000,'hello','join');
+// console.log(Tid)
+// clearTimeout(Tid);
+// let delay= 5000
+// let timeId = setTimeout(() => {
+//   if
+// }, delay);
+// let i = 1
+// // setInterval(function(){
+// //   i++
+// //   console.log(i);
+// // },1000)
+// function func(){
+//   i++
+// }
+// setTimeout(function run()  {
+//   func
+// }, timeout);
+// let start = Date.now()
+// let times = []
+// setTimeout(function run(){
+//   times.push(Date.now()-start)
+//   if(start+100<Date.now()){
+//     console.log(times)
+//   }else{
+//     setTimeout(run);
+//   }
+// })
+// function printNumbers(from, to){
+//  let timer =  setInterval(()=>{
+//     if(from>to){
+//       clearInterval(timer)
+//     }else{
+//       console.log(from);
+//       from++
+//     }
+//   },1000)
+// }
+// printNumbers(1, 4)
+// function printNumbers(from, to){
+//   let timer =  setTimeout(function run(){
+//       if(from>to){
+//         clearInterval(timer)
+//       }else{
+// //         console.log(from);
+// //          timer =  setTimeout(run,1000)
+// //         from++
+
+// //       }
+// //   },1000)
+// //  }
+// let worker = {
+//   someMethod(){
+//     return 1
+//   },
+//   show(min,max){
+//      return min+ max
+//   }
+// }
+
+// function cachingDecorator (func,hash){
+//   let cache = new Map()
+//   return function(x){
+//     console.log(hash(arguments));
+//     let key = hash(arguments)
+//     if(cache.has(key)){
+//       return cache.get(key)
+//     }
+//     let result = func.call(this,...arguments)
+//     cache.set(key,result)
+//     return result
+//   }
+
+// }
+// function hash(args){
+//   return args[0] +','+ args[1]
+// }
+// worker.show = cachingDecorator(worker.show,hash)
+// console.log(worker.show(1,2));
+// console.log(worker.show(1,3));
+// // let fn = worker.show
+// fn(2)
+// fn.call(worker,2)
+
+// function say(){
+//   console.log(this.name);
+// }
+// say.call({name:123});
+// let arr = [].join.call([1,2,3])
+// console.log(arr);
+// function work(a, b) {
+//   console.log(a + b); // work 是一个任意的函数或方法
+// }
+
+// work = spy(work);
+
+// work(1, 2); // 3
+// work(4, 5); // 9
+
+// for (let args of work.calls) {
+//   console.log('call:' + args.join()); // "call:1,2", "call:4,5"
+// }
+
+// function spy(func) {
+
+//   function warpper(...args) {
+//     warpper.calls.push(args)
+//     return func.apply(this, args)
+//   }
+//   warpper.calls = []
+//   return warpper
+// }
+// function f(x) {
+//   console.log(x);
+// }
+
+// // create wrappers
+// let f1000 = delay(f, 1000);
+// let f1500 = delay(f, 1500);
+
+// f1000("test"); // 在 1000ms 后显示 "test"
+// f1500("test"); // 在 1500ms 后显示 "test"
+// function delay(func,ms){
+//    return function (){
+//      setTimeout(() => {
+//        func.call(this,arguments)
+//      }, ms);
+//    }
+// }
+// function throttle(func,ms){
+//   let isThrottled = false,
+//   saveArgs,
+//   saveThis
+//     function warpper (){
+//     if(isThrottled){
+//       saveArgs = arguments;
+//       saveThis = this;
+//       return;
+//     }
+//     this.isThrottled = true
+//     func.apply(this,arguments)
+//     setTimeout(() => {
+//       this.isThrottled = false
+//       if(saveArgs){
+//         warpper.apply(saveArgs,saveThis)
+//         saveArgs = saveThis = null
+//       }
+//     }, ms);
+//   }
+//   return warpper
+// // }
+// let obj = {
+//   user:'join'
+
+// }
+// function func(){
+//   console.log(this.user+'+'+ [...arguments])
+// }
+// let fn = func.bind(obj)
+// console.log(fn);
+// // fn('hello','dhh')
+// let user = {
+//   firstName: 'Hoin',
+//   sayHI(a,b) {
+//     console.log(`hello:${this.firstName}`);
+//     console.log(a*b);
+//   }
+// }
+// let sayHI = user.sayHI.bind(user)
+// setTimeout(() => {
+//   sayHI(10,10)
+// }, 1000);
+// user = {
+//   sayHi() {
+//     console.log("Another user in setTimeout!");
+//   }
+// };
+// let user = {
+//   name:'xc',
+//   say(){
+//     console.log(this.name);
+//   },
+//   eat(){
+//     console.log(this.name)
+//   }
+
+// }
+// for (const key in user) {
+//   if(typeof user[key] ==='function'){
+//     user[key] =  user[key].bind(user)
+//   }
+// }
+// let say  = user.say
+
+// say()
+// let obj = {
+//   name:'wl',
+//   say:say
+// }
+// obj.say()
+// function mul(a,b){
+//   return a*b
+// }
+// let double  = mul.bind(null)
+// console.log(
+// function partial(func,...argsBound){
+//   return function (...args){
+//     return func.call(this,...argsBound,...args)
+//   }
+// }
+// let user = {
+//   firstName:'join',
+//   say(time,phrase){
+//     console.log(`[${time}] ${this.firstName}: ${phrase}!`)
+//   }
+// }
+// user.sayNow = partial(user.say, new Date().getHours() + ':' + new Date().getMinutes())
+// user.sayNow('hello')
+//   let obj = {
+//     name:"wl"
+//   }
+//   // let des = Object.getOwnPropertyDescriptor(obj)
+//   Object.defineProperty(obj,'name',{
+// configurable:false
+//   })
+//   //  obj.name = 'pete'
+//  delete obj.name
+//  let obj = {
+//    name:"john",
+//    surname:'smith',
+//    get propName(){
+//      return `${this.name} ${this.surname}`
+//    },
+//    set propName(value){
+//      [this.name,this.surname] = value.split(
+//        ' '
+//      )
+//    }
+//  }
+//  console.log(obj.propName);
+//  obj.propName = 'wang liang'
+//  console.log(obj.propName);
+// let user = {
+//   name: 'John',
+//   surname: 'Smith'
+// }
+// Object.defineProperty(user, 'fullName', {
+//   get() {
+//     return `${this.name} ${this.surname}`
+//   },
+//   set(value) {
+//     return [this.name, this.surname] = value.split(' ')
+//   }
+// })
+// console.log(user.fullName);
+// user.fullName = 'john bob'
+// console.log(user.fullName);
+// let user = {
+//   get name(){
+//     return this._name
+//   },
+//   set name(value){
+//     if(value.length<4){
+
+//       console.log('value太短了');
+//       return
+//     }
+//     this._name  = value
+//   }
+// }
+// user.name = 'pete'
+// console.log(user.name);
+// user.name = ''
+// // console.log(user.name);
+
+// function User(name, brithday) {
+//   this.name = name;
+//   this.brithday = brithday,
+//     Object.defineProperty(this, 'age', {
+//       get() {
+//         let todayYear = new Date().getFullYear()
+//         return todayYear - this.brithday.getFullYear()
+//       }
+//     })
+// }
+// let john = new User('John',new Date(1998,6,13))
+// console.log(john.brithday);
+// console.log(john.age);
+// let animal = {
+//   eats:true,
+//   walk(){
+//     console.log('animal walk');
+//   }
+// }
+// // let rabbit = {
+// //   jumps:true,
+// //   __proto__:animal
+// // }
+// // rabbit.walk()
+// let user ={
+//   name:'john',
+//   surname:'smith',
+//   set fullName(value){
+//     [this.name, this.surname] = value.split(" ");
+//   },
+
+//   get fullName() {
+//     return `${this.name} ${this.surname}`;
+//   }
+// }
+// let admin = {
+//   __proto__:user,
+//   isAdmin:true
+// }
+// // console.log(admin.fullName);
+// // admin.fullName = 'alice smith'
+// // user.fullName = 'bob smith'
+// // console.log(admin.fullName);
+// // console.log(user.fullName);
+// // console.log(admin);
+// // for (const key in admin) {
+// //   console.log(key);
+// // // }
+// // for (const key in admin) {
+// //   if (admin.hasOwnProperty(key)) {
+// //     console.log(key);//
+// //   }
+// // }
+// let head = {
+//   glasses: 1
+// };
+
+// let table = {
+//   pen: 3,
+//   __proto__:head
+
+// };
+
+// let bed = {
+//   sheet: 1,
+//   pillow: 2,
+//   __proto__:table
+
+// };
+
+// let pockets = {
+//   money: 2000,
+//   __proto__:bed
+// };
+// // console.log(pockets.pen);
+// let hamster = {
+//   stomach: [],
+
+//   eat(food) {
+//     this.stomach.push(food);
+//   }
+// };
+
+// let speedy = {
+//   stomach: [],
+//   __proto__: hamster
+// };
+
+// let lazy = {
+//   stomach: [],
+//   __proto__: hamster
+// };
+
+// // 这只仓鼠找到了食物
+// speedy.eat("apple");
+// console.log( speedy.stomach ); // apple
+
+// // 这只仓鼠也找到了食物，为什么？请修复它。
+// // console.log( lazy.stomach ); // apple
+
+// let obj = {}
+// console.log(obj.__proto__);
+// let animal = {
+//   eats:true
+// }
+// let rabbit = {
+//   name:'xtt'
+// }
+//   rabbit  = Object.create(animal,{
+//   jumps:{
+//     value:true
+//   }
+// })
+// // console.log(rabbit.eats);
+// // console.log(Object.getPrototypeOf(rabbit)===animal);
+// // // console.log(Object.setPrototypeOf(rabbit,{}));
+// // console.log(rabbit.eats);
+// // console.log(rabbit.jumps);
+// console.log(rabbit.name);
+// console.log(Object.keys(rabbit));
+// console.log(Object.values(rabbit));
+// console.log(Object.entries(rabbit));
+// console.log(Object.getPrototypeOf(rabbit));
+// console.log(Reflect.ownKeys(rabbit));
+// for (const key in rabbit) {
+//   console.log(key);
+// }
+// let dictionary = Object.create(null,{
+//   toString:{
+//     value(){
+//       return '123'
+//     }
+//   }
+// });
+
+// // 你的添加 dictionary.toString 方法的代码
+
+// // 添加一些数据
+// dictionary.apple = "Apple";
+// dictionary.__proto__ = "test"; // 这里 __proto__ 是一个常规的属性键
+
+// // 在循环中只有 apple 和 __proto__
+// for(let key in dictionary) {
+//   console.log(key); // "apple", then "__proto__"
+// }
+
+// // 你的 toString 方法在发挥作用
+// console.log(dictionary); // "apple,__proto__"
+// class User{
+//   constructor(name){
+//     this.name = name
+//   }
+//   sayHi(){
+//     console.log(this.name);
+//   }
+// }
+// console.log(typeof User);//function
+// console.log(User===User.prototype.constructor); //true
+// console.log(User.prototype.sayHi);
+// console.log(Object.getOwnPropertyNames(User.prototype));
+// for (const key in User) {
+//   console.log(key);
+// }
+// let obj = {
+
+// }
+//   class User {
+//   constructor(name){
+//     this.name = name
+//   }
+//   get name(){
+//     return this._name
+//   }
+//   set name(val){
+//     this._name  = val
+//   }
+// }
+// let user = new User('wl')
+// // console.log(user.name);
+// user.name = 'xc'
+// console.log(user.name);
+// class Animal{
+//   constructor(name,){
+//     this.speed = 0;
+//     this.name = name
+//   }
+//   run(speed){
+//     this.speed = speed;
+//     console.log(`${this.name} runs whit speed ${this.speed}`);
+//   }
+//   stop(){
+//     this.speed = 0
+//     console.log(`${this.name} stands still`);
+//   }
+  
+// }
+// let animal = new Animal('my animal')
+
+// class Rabbit extends Animal{
+//  constructor(name,earLength){
+//    super(name)
+   
+//    this.earLength = earLength
+//  }
+//   hide(){
+//     console.log(`${this.name} hides `);
+//   }
+//   stop(){
+//     // super.stop()
+//     // this.hide()
+//     setTimeout(()=>{
+//   super.stop()
+//     }, 1000);
+//   }
+// }
+// let rabbit = new Rabbit('whith Rabbit')
+// rabbit.run(5)
+// rabbit.hide()
+// rabbit.stop()
+// console.log(Rabbit.prototype.__proto__===Animal.prototype);
+// console.log(rabbit.__proto__.__proto__===Animal.prototype);//true
+// function f(phrase){
+//   return class {
+//     sayHi(){
+//       console.log(phrase);
+//     }
+//   }
+// }
+// class User extends f('hello'){}
+// let u = new User()
+// u.sayHi()
+// class user{
+//   static say(){
+//     console.log('enne');
+//   }
+//   eat(){
+//     console.log('eat');
+//   }
+// }
+// user.say()
+//  let u = new user()
+//  u.eat()
+//  u.say()
+// class Article {
+//   constructor(title,date){
+//     this.data = date
+//     this.title = title
+//   }
+//   static commpare(article1,article2){
+//     return article1.date - article2.date
+//   }
+// }
+// let articles = [
+//   new Article("HTML", new Date(2019, 1, 1)),
+//   new Article("CSS", new Date(2019, 0, 1)),
+//   new Article("JavaScript", new Date(2019, 11, 1)) 
+// ]
+// articles.sort(Article.commpare)
+// console.log(articles[0].title);
+// class Article {
+//   constructor (title,date){
+//  this.title = title;
+//  this.date = date
+//   }
+//   static createTodays(){
+//     return new this("Today's digest",new Date())
+
+//   }
+// }
+// let article = Article.createTodays()
+// console.log(article.title);
+// console.log(article.date);
+// class Animal {
+//   static plant = "earth"
+//   constructor(name,speed) {
+//     this.name = name;
+//     this.speed = speed
+//   }
+//   run(speed =0){
+//     this.speed += speed;
+//     console.log(`${this.name} runs with speed ${this.speed}.`);
+//   }
+//   static compare(animal1,animal2){
+//     return animal1.speed - animal2.speed
+//   }
+// }
+
+// class Rabbit extends Animal {
+//   hide(){
+//     console.log('hide');
+//   }
+// }
+// let rabbits =[new Rabbit('白兔子',5),new Rabbit('黑兔子',10)]
+// // rabbits.sort(Rabbit.compare)
+// // rabbits[0].run()
+// // console.log(Rabbit.plant);
+// console.log(rabbits[0].__proto__);
+// console.log(Rabbit.__proto__);
+// // console.log(Animal.__proto__);
+// console.log('异步加载');
+// function newFn(){
+//   console.log('我是newfn');
+// }
+let obj = {
+  name:'xl',
+  age:12
+}
+let arr = [1,2,3,4]
+for (const key in obj) {
+console.log(key);
+}
+ 
+for (const iterator of arr) {
+  console.log(iterator);
 }
